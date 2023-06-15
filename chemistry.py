79d5dd460e02d4ce0ba225c9930cbfe943223123
lato <105388681+l4t8@users.noreply.github.com>
2023-06-15 20:21:10 +0200

    Second commit
    The engine is able of naming the properties of the compounds, pretty basic stuff for now

--- chemistry.py
+++ chemistry.py
@@ -1,28 +1,46 @@
 elements = ['null', 'Hydrogen', 'Helium', 'Lithium', 'Beryllium', 'Boron', 'Carbon', 'Nitrogen', 'Oxygen', 'Fluorine', 'Neon', 'Sodium', 'Magnesium', 'Aluminum', 'Silicon', 'Phosphorus', 'Sulfur', 'Chlorine', 'Argon', 'Potassium', 'Calcium', 'Scandium', 'Titanium', 'Vanadium', 'Chromium', 'Manganese', 'Iron', 'Cobalt', 'Nickel', 'Copper', 'Zinc', 'Gallium', 'Germanium', 'Arsenic', 'Selenium', 'Bromine', 'Krypton', 'Rubidium', 'Strontium', 'Yttrium', 'Zirconium', 'Niobium', 'Molybdenum', 'Technetium', 'Ruthenium', 'Rhodium', 'Palladium', 'Silver', 'Cadmium', 'Indium', 'Tin', 'Antimony', 'Tellurium', 'Iodine', 'Xenon', 'Cesium', 'Barium', 'Lanthanum', 'Cerium', 'Praseodymium', 'Neodymium', 'Promethium', 'Samarium', 'Europium', 'Gadolinium', 'Terbium', 'Dysprosium', 'Holmium', 'Erbium', 'Thulium', 'Ytterbium', 'Lutetium', 'Hafnium', 'Tantalum', 'Wolfram', 'Rhenium', 'Osmium', 'Iridium', 'Platinum', 'Gold', 'Mercury', 'Thallium', 'Lead', 'Bismuth', 'Polonium', 'Astatine', 'Radon', 'Francium', 'Radium', 'Actinium', 'Thorium', 'Protactinium', 'Uranium', 'Neptunium', 'Plutonium', 'Americium', 'Curium', 'Berkelium', 'Californium', 'Einsteinium', 'Fermium', 'Mendelevium', 'Nobelium', 'Lawrencium', 'Rutherfordium', 'Dubnium', 'Seaborgium', 'Bohrium', 'Hassium', 'Meitnerium', 'Darmstadtium', 'Roentgenium', 'Copernicium', 'Nihonium', 'Flerovium', 'Moscovium', 'Livermorium', 'Tennessine', 'Oganesson']
 abreviations = ['null','H', 'He', 'Li', 'Be', 'B', 'C', 'N', 'O', 'F', 'Ne', 'Na', 'Mg', 'Al', 'Si', 'P', 'S', 'Cl', 'Ar', 'K', 'Ca', 'Sc', 'Ti', 'V', 'Cr', 'Mn', 'Fe', 'Co', 'Ni', 'Cu', 'Zn', 'Ga', 'Ge', 'As', 'Se', 'Br', 'Kr', 'Rb', 'Sr', 'Y', 'Zr', 'Nb', 'Mo', 'Tc', 'Ru', 'Rh', 'Pd', 'Ag', 'Cd', 'In', 'Sn', 'Sb', 'Te', 'I', 'Xe', 'Cs', 'Ba', 'La', 'Ce', 'Pr', 'Nd', 'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb', 'Lu', 'Hf', 'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg', 'Tl', 'Pb', 'Bi', 'Po', 'At', 'Rn', 'Fr', 'Ra', 'Ac', 'Th', 'Pa', 'U', 'Np', 'Pu', 'Am', 'Cm', 'Bk', 'Cf', 'Es', 'Fm', 'Md', 'No', 'Lr', 'Rf', 'Db', 'Sg', 'Bh', 'Hs', 'Mt', 'Ds', 'Rg', 'Cn', 'Nh', 'Fl', 'Mc', 'Lv', 'Ts', 'Og']
-masses = [0, 1.0079, 4.0026, 6.941, 9.0122, 10.811, 12.0107, 14.0067, 15.9994, 18.9984, 20.1797, 22.9897, 24.305, 26.9815, 28.0855, 30.9738, 32.065, 35.453, 39.0983, 39.948, 40.078, 44.9559, 47.867, 50.9415, 51.9961, 54.938, 55.845, 58.6934, 58.9332, 63.546, 65.39, 69.723, 72.64, 74.9216, 78.96, 79.904, 83.8, 85.4678, 87.62, 88.9059, 91.224, 92.9064, 95.94, 98.0, 101.07, 102.9055, 106.42, 107.8682, 112.411, 114.818, 118.71, 50121.76, 126.9045, 127.6, 131.293, 132.9055, 137.327, 138.9055, 140.116, 140.9077, 144.24, 145.0, 150.36, 151.964, 157.25, 158.9253, 162.5, 164.9303, 167.259, 168.9342, 173.04, 174.967, 178.49, 180.9479, 183.84, 186.207, 190.23, 192.217, 195.078, 196.9665, 200.59, 204.3833, 207.2, 208.9804, 209.0, 210.0, 222.0, 223.0, 226.0, 227.0, 231.0359, 232.0381, 237.0, 238.0289, 243.0, 244.0, 247.0, 247.0, 251.0, 252.0, 257.0, 258.0, 259.0, 261.0, 262.0, 262.0, 264.0, 266.0, 268.0, 272.0, 277.0]
-valenceshell = [[], [1], [0], [1], [2], [-3,3], [-4,2,4], [-3,1,2,3,4,5], [-2], [-1], [0], [1], [2], [3], [-4,2,4], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], 
-[], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], [], []]
+masses = [0, 1.0079, 4.0026, 6.941, 9.0122, 10.811, 12.0107, 14.0067, 15.9994, 18.9984, 20.1797, 22.9897, 24.305, 26.9815, 28.0855, 30.9738, 32.065, 35.453, 39.0983, 39.948, 40.078, 44.9559, 47.867, 50.9415, 51.9961, 54.938, 55.845, 58.6934, 58.9332, 63.546, 65.39, 69.723, 72.64, 74.9216, 78.96, 79.904, 83.8, 85.4678, 87.62, 88.9059, 91.224, 92.9064, 95.94, 98.0, 101.07, 102.9055, 106.42, 107.8682, 112.411, 114.818, 118.71, 50121.76, 126.9045, 127.6, 131.293, 132.9055, 137.327, 138.9055, 140.116, 140.9077, 144.24, 145.0, 150.36, 151.964, 157.25, 158.9253, 162.5, 164.9303, 167.259, 168.9342, 173.04, 174.967, 178.49, 180.9479, 183.84, 186.207, 190.23, 192.217, 195.078, 196.9665, 200.59, 204.3833, 207.2, 208.9804, 209.0, 210.0, 222.0, 223.0, 226.0, 227.0, 231.0359, 232.0381, 237.0, 238.0289, 243.0, 244.0, 247.0, 247.0, 251.0, 252.0, 257.0, 258.0, 259.0, 261.0, 262.0, 262.0, 264.0, 266.0, 268.0, 272.0, 277.0,"incomprehensible","incomprehensible","incomprehensible","incomprehensible","incomprehensible","incomprehensible","incomprehensible","incomprehensible"]
+oxidation_numbers = ['null', [1], [0], [1], [2], [-3,3], [-4,2,4], [-3,1,2,3,4,5], [-2], [-1], [0], [1], [2], [3], [-4,2,4], [-3,3,5],
+[-2,2,4,6], [-1,1,3,5,7], [0], [1], [2], [3], [2,3,4], [2,3,4,5], [2,3,6], [2,3,4,6,7], [2,3], [2,3], [2,3], [1,2], [2], [1,3], 
+[2,4], [-3,3,5], [-2,4,6], [-1,1,3,5,7], [0], [1], [2], [3], [3,4], [2,3,4,5],[2,3,4,5,6], [4,5,6,7], [2,3,4,5,6,7,8], [2,3,4,5,6], 
+[2,4], [1], [2], [1,3], [2,4], [-3,3,5], [-2,2,4,6], [-1,1,3,5,7], [0], [1], [2], [2,3],
+[2,3,4], [2,3,4], [2,3], [3], [2,3], [2,3], [1,2,3], [1,3,4], [2,3], [3], [3], [2,3], [2,3], [3], [2,3,4], [2,3,4,5], [1,2,3,4,5,6], [1,2,3,4,5,6,7], 
+[1,2,3,4,5,6,7,8], [1,2,3,4,5,6], [2,4,5,6], [1,2,3,5], [1,2,4], [1,3], [2,4], [3,5], [2,4,6], [-1,1], [0], [1], [2], [3],
+[2,3,4], [3,4,5], [3,4,5,6], [3,4,5,6,7], [3,4,5,6,7], [2,3,4,5,6], [3,4],[3,4], [1,3,4], [2,3], [2,3], [2,3], [2,3], [3], [4], [5], [6], [7], [8],
+"incomprehensible","incomprehensible","incomprehensible","incomprehensible","incomprehensible","incomprehensible","incomprehensible","incomprehensible","incomprehensible","incomprehensible"]
+elecconfig = ['0','1s1','1s2','2s1','2s2','2p1','2p2','2p3','2p4','2p5','2p6','3s1','3s2','3p1','3p2','3p3','3p4','3p5','3p6','4s1','4s2','3d1','3d2','3d3','3d4','3d5','3d6','3d7','3d8','3d9','3d10','4p1','4p2','4p3','4p4','4p5','4p6','5s1','5s2','4d1','4d2','4d3','4d4','4d5','4d6','4d7','4d8','4d9','4d10','5p1','5p2','5p3','5p4','5p5','5p6','6s1','6s2','4f1','4f2','4f3','4f4','4f5','4f6','4f7','4f8','4f9','4f10','4f11','4f12','4f13','4f14','5d1','5d2','5d3','5d4','5d5','5d6','5d7','5d8','5d9','5d10','6p1','6p2','6p3','6p4','6p5','6p6','7s1','7s2','5f1','5f2','5f3','5f4','5f5','5f6','5f7','5f8','5f9','5f10','5f11','5f12','5f13','5f14','6d1','6d2','6d3','6d4','6d5','6d6','6d7','6d8','6d9','6d10','7p1','7p2','7p3','7p4','7p5','7p6']
 
 def getobject(z):
     atom(z)
 
 class atom:
+
     def __init__(self, z):
+
         self.z = z
         self.name = elements[z]
         self.abreviation = abreviations[z]
-        self.mass = masses[z]
-        
+        self.molarmass = masses[z]
+        self.oxinums = oxidation_numbers[z]
+        self.elecconfig = elecconfig[z]
 
-#class metalloid(atom):
-
-#class nonmetal(atom):
-
-#class metal(atom):
+        #Getting the element type
+        if self.elecconfig[1:] == "p6" or self.name == 'Helium':
+            self.elementype = 'Noble gas'
+        elif self.name in ['Boron','Silicon','Germanium','Arsenic','Antimony','Tellurium','Polonium']:
+            self.elementype = 'Metalloid'
+        elif self.elecconfig in ['2p2','2p3','2p4','2p5','3p3','3p4','3p5','4p4','4p5','5p5','6p5']:
+            self.elementype = 'Nonmetal'
+        elif self.name == 'Hydrogen':
+            self.elementype = 'Hydrogen'
+        else:
+            self.elementype = "Metal"
 
 class bond:
+    
     def __init__(self):
+
         pass
 
 class ionic(bond):
@@ -31,16 +49,32 @@ class ionic(bond):
         self.anion = anion
         self.cation = cation
 
-    def get_name(self):
-        
-        return self.anion.name + self.cation.name
 
 class organic(bond):
-    def __init__(self):
-        super().__init__(self)
-        pass
-     
 
+    def __init__(self,elements):
+        
+        self.elements = elements
 
+def compound_names(a,b):
 
-print(ionic(atom(1),atom(3)).get_name())
+    return a + b
+
+print("Hello there! This is a primitive chemistry engine. Search for the basic properties of elements or the compound names of an ionic bond!")
+theput =  input('Insert "bond" for getting the name of an ionic compound, or "properties" for searching for properties: ')
+if theput == "bond":
+    a = input("Now insert the first element, for example: Na2")
+    b = input("Now insert the second element, for example: O")
+    print(compound_names(a,b))
+elif theput == "properties":
+    theput1 = input("Perfect! Now insert the full name of the element:  ")
+    theput2 = input('Insert one of the following properties to research for "molarmass" "oxidation numbers" , "electric configuration" or "atomic number" :  ')
+    if theput2 == "molar mass":
+        print(f"The {theput2} of {theput1} is {atom(elements.index(theput1)).molarmass}")
+    elif theput2 == "oxidation numbers":
+        print(f"The {theput2} of {theput1} are {atom(elements.index(theput1)).oxinums}")
+    elif theput2 == "electric configuration":
+        print(f"The {theput2} of {theput1} is {atom(elements.index(theput1)).elecconfig}")
+    elif theput2 == "atomic number":
+        print(f"The {theput2} of {theput1} is {elements.index(theput1)}")
+    
